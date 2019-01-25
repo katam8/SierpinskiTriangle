ddae791b446b15a96a43e994ed937c81e254c188
katam8 <philliptam986@gmail.com>
2019-01-25 13:39:41 -0800

    finish with a clean cut

--- out/source/SierpinskiTriangle.java
+++ out/source/SierpinskiTriangle.java
@@ -14,6 +14,10 @@ import java.io.IOException;
 
 public class SierpinskiTriangle extends PApplet {
 
+int length = 200;
+int clr1;
+int clr2;
+int clr3;
 public void setup()
 {
     
@@ -22,25 +26,28 @@ public void setup()
 
 public void draw()
 {
-    /*
-    int clr1 = (int)(Math.random()*255 + 1);
-    int clr2 = (int)(Math.random()*255 + 1);
-    int clr3 = (int)(Math.random()*255 + 1);
-    */
-    sierpinski(2, height - 2, height);
+    sierpinski(2, height - 2, length);
 }
 
 public void mouseClicked() {
-    int clr1 = (int)(Math.random()*255 + 1);
-    int clr2 = (int)(Math.random()*255 + 1);
-    int clr3 = (int)(Math.random()*255 + 1);
-    fill(clr1, clr2, clr3);
+    fill(0,0,0);
+    rect(0, 0, width, height);
+    clr1 = (int)(Math.random()*255 + 1);
+    clr2 = (int)(Math.random()*255 + 1);
+    clr3 = (int)(Math.random()*255 + 1);
+    //fill(clr1, (int)((float)(900-y)/900*255), clr3);
+    if(length == 200) {
+        length = 400;
+    } else if(length == 400) {
+        length = 600;
+    } else {}
 }
 
 public void sierpinski(int x, int y, int len) 
 {
+    fill((int)((float)(900-y)/900*255), clr2, clr3);
     noStroke();
-    if (len <= 20)
+    if (len < 20)
         triangle(x, y, x + len, y, x + len / 2, y - len);
     else {
         sierpinski(x, y, len / 2);
